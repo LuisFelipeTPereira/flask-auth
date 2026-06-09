c52a0b11bc5df97c5a3a9e3dd79e12844362e31b
Luis Felipe <lfelipetpereira@gmail.com>
2026-06-09 14:43:28 -0300

    Implementing Authentication & CRUD Users

---
+++ models/user.py
@@ -0,0 +1,9 @@
+from database import db
+from flask import Flask
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    # id(int), username(text), password(text)
+    id = db.Column(db.Integer, primary_key=True) #chave primária que identifica os registros na tabela(ela é única)
+    username = db.Column(db.String(80), nullable=False,unique=True) #nullable mostra se eu posso deixar em branco ou não aquele item da tabela
+    password = db.Column(db.String(80), nullable=False)
